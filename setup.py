a6195bfe8e77c8faafd202fb11831d0aa76a9ef3
Christian Theune <ct@flyingcircus.io>
2010-04-12 16:12:49 +0200

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.workflow',
-      version = '0.1',
+      version = '0.2dev',
       description="Workflow extension for the Assembly CMS",
       author="Webcrew",
       author_email="web@assembly.org",
